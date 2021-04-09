8c9953cb098aaa209b2df7aec67b8f6b0b38c0bc
i356100@fhict.nl <n.vandriel@student.fontys.nl>
2021-04-09 12:40:22 +0200

    fixed online  version
    probleem opgelost waarin de online versie  de serviceworker niet kon vinden

--- sw.js
+++ sw.js
@@ -7,5 +7,15 @@ self.addEventListener("install", e =>{
             return cache.addAll(["./", "./src/master.css", "./images/logo192.png"]);
         })
     );
-    console.log("install!");   
+    console.log("installed!");   
+});
+
+self.addEventListener("fetch", e =>{
+    e.respondWith(
+        caches.match(e.request).then(response =>{
+            return response || fetch(e.request);
+        })
+    );
+
+    console.log(`Interscepting fetch request for: ${e.request.url}`);
 });
